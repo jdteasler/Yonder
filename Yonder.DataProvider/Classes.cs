7fc1c1e5e5050d52526ae966b54a891c4382fe1e
John Teasler <jteasler@gmail.com>
2020-11-03 23:44:06 -0600

    feat: Allow users to Create a new class and display class information.

--- Yonder.DataProvider/Classes.cs
+++ Yonder.DataProvider/Classes.cs
@@ -8,7 +8,6 @@ namespace Yonder.DataProvider
 {
     public class Classes
     {
-        [Required]
         public int Id { get; set; }
         public string ClassName { get; set; }
         public string Description { get; set; }
